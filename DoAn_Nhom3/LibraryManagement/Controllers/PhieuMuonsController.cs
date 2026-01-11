a37b28bda160c00eb141042e9ffb1902068fe4ec
letienhoang <ltienhoang2@gmail.com>
2026-01-11 18:06:09 +0700

    update PhieuMuonsController view

--- DoAn_Nhom3/LibraryManagement/Controllers/PhieuMuonsController.cs
+++ DoAn_Nhom3/LibraryManagement/Controllers/PhieuMuonsController.cs
@@ -1,96 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LibraryManagement.Models;
+using LibraryManagement.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LibraryManagement.Controllers
 {
     public class PhieuMuonsController : Controller
     {
         private readonly LibraryDbContext _context;
+        private readonly ILibraryCodeGenerator _codeGen;
 
-        public PhieuMuonsController(LibraryDbContext context)
+        public PhieuMuonsController(LibraryDbContext context, ILibraryCodeGenerator codeGen)
         {
             _context = context;
+            _codeGen = codeGen;
         }
 
         // GET: PhieuMuons
         public async Task<IActionResult> Index()
         {
-            return View(await _context.PhieuMuons.ToListAsync());
+            var items = await _context.PhieuMuons
+                .Include(p => p.NguoiMuon)
+                .Include(p => p.NhanVien)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(items);
         }
 
         // GET: PhieuMuons/Details/5
-        public async Task<IActionResult> Details(string id)
+        public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var phieuMuon = await _context.PhieuMuons
-                .FirstOrDefaultAsync(m => m.MaPhieuMuon == id);
-            if (phieuMuon == null)
-            {
-                return NotFound();
-            }
+                .Include(p => p.NguoiMuon)
+                .Include(p => p.NhanVien)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (phieuMuon == null) return NotFound();
 
             return View(phieuMuon);
         }
 
         // GET: PhieuMuons/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            return View();
+            var model = new PhieuMuon
+            {
+                MaPhieuMuon = await _codeGen.GenerateNextAsync<PhieuMuon>(p => p.MaPhieuMuon, "PM", 4),
+                NgayMuon = DateTime.Now,
+                HanTra = DateTime.Now.AddDays(14)
+            };
+
+            await PopulateSelectsAsync();
+            ViewBag.GeneratedMaPhieuMuon = model.MaPhieuMuon;
+            return View(model);
         }
 
         // POST: PhieuMuons/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MaPhieuMuon,MaNguoiMuon,MaNhanVien,NgayMuon,HanTra,TrangThai,SoNgayTre")] PhieuMuon phieuMuon)
+        public async Task<IActionResult> Create([Bind("MaPhieuMuon,NguoiMuonId,NhanVienId,NgayMuon,HanTra,TrangThai,SoNgayTre")] PhieuMuon phieuMuon)
         {
+            // ensure unique code with retries
+            phieuMuon.MaPhieuMuon = await _codeGen.GenerateNextWithRetriesAsync<PhieuMuon>(p => p.MaPhieuMuon, "PM", 4);
+
             if (ModelState.IsValid)
             {
                 _context.Add(phieuMuon);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
+            await PopulateSelectsAsync();
+            ViewBag.GeneratedMaPhieuMuon = phieuMuon.MaPhieuMuon;
             return View(phieuMuon);
         }
 
         // GET: PhieuMuons/Edit/5
-        public async Task<IActionResult> Edit(string id)
+        public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var phieuMuon = await _context.PhieuMuons.FindAsync(id);
-            if (phieuMuon == null)
-            {
-                return NotFound();
-            }
+            if (phieuMuon == null) return NotFound();
+
+            await PopulateSelectsAsync(phieuMuon.NguoiMuonId, phieuMuon.NhanVienId);
             return View(phieuMuon);
         }
 
         // POST: PhieuMuons/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("MaPhieuMuon,MaNguoiMuon,MaNhanVien,NgayMuon,HanTra,TrangThai,SoNgayTre")] PhieuMuon phieuMuon)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MaPhieuMuon,NguoiMuonId,NhanVienId,NgayMuon,HanTra,TrangThai,SoNgayTre")] PhieuMuon phieuMuon)
         {
-            if (id != phieuMuon.MaPhieuMuon)
-            {
-                return NotFound();
-            }
+            if (id != phieuMuon.Id) return NotFound();
 
             if (ModelState.IsValid)
             {
@@ -101,34 +108,30 @@ namespace LibraryManagement.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!PhieuMuonExists(phieuMuon.MaPhieuMuon))
-                    {
+                    if (!PhieuMuonExists(phieuMuon.Id))
                         return NotFound();
-                    }
                     else
-                    {
                         throw;
-                    }
                 }
                 return RedirectToAction(nameof(Index));
             }
+
+            await PopulateSelectsAsync(phieuMuon.NguoiMuonId, phieuMuon.NhanVienId);
             return View(phieuMuon);
         }
 
         // GET: PhieuMuons/Delete/5
-        public async Task<IActionResult> Delete(string id)
+        public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var phieuMuon = await _context.PhieuMuons
-                .FirstOrDefaultAsync(m => m.MaPhieuMuon == id);
-            if (phieuMuon == null)
-            {
-                return NotFound();
-            }
+                .Include(p => p.NguoiMuon)
+                .Include(p => p.NhanVien)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (phieuMuon == null) return NotFound();
 
             return View(phieuMuon);
         }
@@ -136,21 +139,44 @@ namespace LibraryManagement.Controllers
         // POST: PhieuMuons/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var phieuMuon = await _context.PhieuMuons.FindAsync(id);
             if (phieuMuon != null)
             {
                 _context.PhieuMuons.Remove(phieuMuon);
+                await _context.SaveChangesAsync();
             }
-
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool PhieuMuonExists(string id)
+        private bool PhieuMuonExists(int id) =>
+            _context.PhieuMuons.Any(e => e.Id == id);
+
+        private async Task PopulateSelectsAsync(int? selectedNguoiId = null, int? selectedNhanVienId = null)
         {
-            return _context.PhieuMuons.Any(e => e.MaPhieuMuon == id);
+            var nguoiList = await _context.NguoiMuons
+                .AsNoTracking()
+                .OrderBy(n => n.HoTen)
+                .Select(n => new SelectListItem
+                {
+                    Value = n.Id.ToString(),
+                    Text = string.IsNullOrWhiteSpace(n.HoTen) ? n.MaNguoiMuon : $"{n.HoTen} ({n.MaNguoiMuon})"
+                })
+                .ToListAsync();
+
+            var nhanvienList = await _context.NhanViens
+                .AsNoTracking()
+                .OrderBy(n => n.HoTen)
+                .Select(n => new SelectListItem
+                {
+                    Value = n.Id.ToString(),
+                    Text = string.IsNullOrWhiteSpace(n.HoTen) ? n.MaNhanVien : $"{n.HoTen} ({n.MaNhanVien})"
+                })
+                .ToListAsync();
+
+            ViewBag.NguoiMuonId = new SelectList(nguoiList, "Value", "Text", selectedNguoiId?.ToString());
+            ViewBag.NhanVienId = new SelectList(nhanvienList, "Value", "Text", selectedNhanVienId?.ToString());
         }
     }
 }
---
+++ DoAn_Nhom3/LibraryManagement/Extensions/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace LibraryManagement.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum? value)
+    {
+        if (value == null) return string.Empty;
+
+        var member = value.GetType().GetMember(value.ToString()).FirstOrDefault();
+        if (member == null) return value.ToString();
+
+        var displayAttr = member.GetCustomAttribute<DisplayAttribute>();
+        return displayAttr?.GetName() ?? value.ToString();
+    }
+}
