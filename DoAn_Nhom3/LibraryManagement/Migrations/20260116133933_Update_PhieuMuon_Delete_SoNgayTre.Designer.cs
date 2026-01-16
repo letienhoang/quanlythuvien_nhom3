a8f50b9653d7d37e1b9481f4c153ec423e1d6a94
ntphi360 <ntphi360@gmail.com>
2026-01-16 22:07:18 +0700

    fix view_sachs

--- DoAn_Nhom3/LibraryManagement/Migrations/20260116133933_Update_PhieuMuon_Delete_SoNgayTre.Designer.cs
+++ DoAn_Nhom3/LibraryManagement/Migrations/20260116133933_Update_PhieuMuon_Delete_SoNgayTre.Designer.cs
@@ -4,6 +4,7 @@ using LibraryManagement.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryManagement.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260116133933_Update_PhieuMuon_Delete_SoNgayTre")]
+    partial class Update_PhieuMuon_Delete_SoNgayTre
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
